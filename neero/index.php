a46577608e4dd0b18c90371b913ad16ec523a4a6
NikitaShirokih <scop.nik@gmail.com>
2025-06-16 12:33:20 +0500

    test1

--- neero/index.php
+++ neero/index.php
@@ -1,8 +1,14 @@
 <?php
-$host = "localhost";
-$dbname = "test";
-$username = "root";
-$password = "MyPass123!";
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+
+$host = $_ENV['DB_HOST'];
+$dbname = $_ENV['DB_NAME'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASSWORD'];
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
